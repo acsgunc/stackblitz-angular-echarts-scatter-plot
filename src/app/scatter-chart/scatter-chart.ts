9d27637a7d203116484ae793e9e455804ad492b3
acsgunc <git@stackblitz.com>
2025-06-05 10:45:37 +0800

    Initial commit

---
+++ src/app/scatter-chart/scatter-chart.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scatter-chart',
+  imports: [],
+  templateUrl: './scatter-chart.html',
+  styleUrl: './scatter-chart.css'
+})
+export class ScatterChart {
+
+}
